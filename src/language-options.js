2ac826e196ef81b4d97b2d17f89bdd5461b15ab4
Theo Langstraat <theo.langstraat@outlook.com>
2026-06-08 16:36:45 +0200

    chore: clean up

---
+++ src/language-options.js
@@ -0,0 +1,245 @@
+export const languageOptions = {
+	en: [
+		{ version: 'English Standard Version', url: 'esv' },
+		{ version: 'King James Version', url: 'kjv' },
+		{ version: 'New American Standard Bible 2020', url: 'nasb' },
+		{ version: 'New Catholic Bible', url: 'ncb' },
+		{ version: 'New International Version', url: 'niv' },
+		{ version: 'New King James Version', url: 'nkjv' },
+		{ version: 'New Living Translation', url: 'nlt' },
+		{ version: 'New Revised Standard Version', url: 'nrsv' },
+		{ version: 'World English Bible', url: 'web' },
+	],
+	es: [
+		{ version: 'La Biblia de las Américas', url: 'lbla' },
+		{ version: 'Nueva Versión Internacional', url: 'nvi' },
+		{ version: 'Reina-Valera 1960', url: 'rvr60' },
+		{ version: 'Reina-Valera 1995', url: 'rvr95' },
+	],
+	pt: [
+		{ version: 'Almeida Revista e Atualizada', url: 'ara' },
+		{ version: 'Almeida Revista e Corrigida', url: 'arc' },
+		{ version: 'Nova Versão Internacional', url: 'nvi_pt' },
+	],
+	de: [
+		{ version: 'Elberfelder Bibel', url: 'elb' },
+		{ version: 'Luther 1912', url: 'lu12' },
+		{ version: 'Luther 2017', url: 'lut' },
+		{ version: 'Neue evangelistische Übersetzung', url: 'neu' },
+	],
+	fr: [
+		{ version: 'Bible du Semeur', url: 'bds' },
+		{ version: 'Segond 21', url: 'sg21' },
+	],
+	it: [
+		{ version: 'Conferenza Episcopale Italiana', url: 'cei' },
+		{ version: 'Nuova Riveduta 2006', url: 'nr06' },
+	],
+	pl: [
+		{ version: 'Biblia Warszawska 1975', url: 'bw1975' },
+		{ version: 'Uwspółcześniona Biblia Gdańska', url: 'ubg' },
+	],
+	ar: [
+		{ version: 'الكتاب المقدس', url: 'avd' },
+		{ version: 'ترجمة كتاب الحياة', url: 'keh' },
+	],
+	nl: [
+		{ version: 'BasisBijbel', url: 'bb' },
+		{ version: 'Bijbel in Gewone Taal', url: 'bgt' },
+		{ version: 'Herziene Statenvertaling', url: 'hsv' },
+		{ version: 'Nederlands Bijbelgenootschap', url: 'nbg' },
+		{ version: 'Nieuwe Bijbelvertaling 2021', url: 'nbv' },
+	],
+	af: [
+		{ version: 'Afrikaans 1933/1953', url: 'afr53' },
+	],
+	bn: [
+		{ version: 'পবিত্র বাইবেল (কেরী ভার্সন)', url: 'rovu' },
+	],
+	zh: [
+		{ version: '和合本聖經', url: 'cuv' },
+		{ version: '和合本圣经', url: 'cuvs' },
+	],
+	cs: [
+		{ version: 'Bible 21', url: 'b21' },
+		{ version: 'Český ekumenický překlad', url: 'cep' },
+	],
+	da: [
+		{ version: 'Bibelen på Hverdagsdansk', url: 'bdan' },
+		{ version: 'Danske Bibel 1871/1907', url: 'da1871' },
+	],
+	fi: [
+		{ version: 'Kirkkoraamattu 1933/1938', url: 'kr38' },
+		{ version: 'Raamattu 1992', url: 'kr92' },
+	],
+	el: [
+		{ version: 'Η Αγία Γραφή', url: 'tgv' },
+	],
+	hi: [
+		{ version: 'पवित्र बाइबिल', url: 'hhbd' },
+	],
+	hu: [
+		{ version: 'Magyar Bibliatársulat újfordítású Bibliája', url: 'uf' },
+	],
+	mg: [
+		{ version: 'Malagasy 1865', url: 'mg1865' },
+	],
+	fa: [
+		{ version: 'کتاب مقدس، ترجمۀ معاصر', url: 'pcb' },
+	],
+	ru: [
+		{ version: 'Синодальный перевод', url: 'rst' },
+	],
+	sk: [
+		{ version: 'Katolícky preklad', url: 'kat' },
+	],
+	st: [
+		{ version: 'BIBELE', url: 'sso89' },
+	],
+	ur: [
+		{ version: 'کِتابِ مُقادّس', url: 'urd' },
+	],
+	xh: [
+		{ version: 'IBHAYIBHILE', url: 'xho96' },
+	],
+	zu: [
+		{ version: 'Zulu 1959', url: 'zul59' },
+	],
+}
+
+export const languageLabels = {
+	en: { native: 'English', english: 'English' },
+	es: { native: 'Español', english: 'Spanish' },
+	pt: { native: 'Português', english: 'Portuguese' },
+	de: { native: 'Deutsch', english: 'German' },
+	fr: { native: 'Français', english: 'French' },
+	it: { native: 'Italiano', english: 'Italian' },
+	pl: { native: 'Polski', english: 'Polish' },
+	ar: { native: 'العربية', english: 'Arabic' },
+	nl: { native: 'Nederlands', english: 'Dutch' },
+	af: { native: 'Afrikaans', english: 'Afrikaans' },
+	bn: { native: '(বাংলা)', english: 'Bengali' },
+	zh: { native: '中文', english: 'Chinese' },
+	cs: { native: 'Čeština', english: 'Czech' },
+	da: { native: 'Dansk', english: 'Danish' },
+	fi: { native: 'Suomi', english: 'Finnish' },
+	el: { native: 'Ελληνικά', english: 'Greek' },
+	hi: { native: 'हिन्दी', english: 'Hindi' },
+	hu: { native: 'Magyar', english: 'Hungarian' },
+	mg: { native: 'Malagasy', english: 'Malagasy' },
+	fa: { native: 'فارسی', english: 'Persian' },
+	ru: { native: 'Русский', english: 'Russian' },
+	sk: { native: 'Slovenčina', english: 'Slovak' },
+	st: { native: 'Sesotho', english: 'Southern Sotho' },
+	ur: { native: 'اردو', english: 'Urdu' },
+	xh: { native: 'isiXhosa', english: 'Xhosa' },
+	zu: { native: 'isiZulu', english: 'Zulu' },
+}
+
+export const versionLabels = {
+	// English
+	esv: 'English Standard Version (ESV)',
+	kjv: 'King James Version (KJV)',
+	nasb: 'New American Standard Bible 2020 (NASB)',
+	ncb: 'New Catholic Bible (NCB)',
+	niv: 'New International Version (NIV)',
+	nkjv: 'New King James Version (NKJV)',
+	nlt: 'New Living Translation (NLT)',
+	nrsv: 'New Revised Standard Version (NRSV)',
+	web: 'World English Bible (WEB)',
+
+	// Spanish
+	lbla: 'La Biblia de las Américas (LBLA)',
+	nvi: 'Nueva Versión Internacional (NVI)',
+	rvr60: 'Reina-Valera 1960 (RVR60)',
+	rvr95: 'Reina-Valera 1995 (RVR95)',
+
+	// Portuguese
+	ara: 'Almeida Revista e Atualizada (ARA)',
+	arc: 'Almeida Revista e Corrigida (ARC)',
+	nvi_pt: 'Nova Versão Internacional (NVI)',
+
+	// German
+	elb: 'Elberfelder Bibel (ELB)',
+	lu12: 'Luther 1912 (LU12)',
+	lut: 'Luther 2017 (LUT)',
+	neu: 'Neue evangelistische Übersetzung (NEÜ)',
+
+	// French
+	bds: 'Bible du Semeur (BDS)',
+	sg21: 'Segond 21 (SG21)',
+
+	// Italian
+	cei: 'Conferenza Episcopale Italiana (CEI)',
+	nr06: 'Nuova Riveduta 2006 (NR06)',
+
+	// Polish
+	bw1975: 'Biblia Warszawska 1975 (BW1975)',
+	ubg: 'Uwspółcześniona Biblia Gdańska (UBG)',
+
+	// Arabic
+	avd: 'الكتاب المقدس (AVD)',
+	keh: 'ترجمة كتاب الحياة (KEH)',
+
+	// Dutch
+	bb: 'BasisBijbel (BB)',
+	bgt: 'Bijbel in Gewone Taal (BGT)',
+	hsv: 'Herziene Statenvertaling (HSV)',
+	nbg: 'Nederlands Bijbelgenootschap (NBG)',
+	nbv: 'Nieuwe Bijbelvertaling 2021 (NBV)',
+
+	// Afrikaans
+	afr53: 'Afrikaans 1933/1953 (AFR53)',
+
+	// Bengali
+	rovu: 'পবিত্র বাইবেল (ROVU)',
+
+	// Chinese
+	cuv: '和合本聖經 (CUV)',
+	cuvs: '和合本圣经 (CUVS)',
+
+	// Czech
+	b21: 'Bible 21 (B21)',
+	cep: 'Český ekumenický překlad (ČEP)',
+
+	// Danish
+	bdan: 'Bibelen på Hverdagsdansk (BDAN)',
+	da1871: 'Danske Bibel 1871/1907 (DA1871)',
+
+	// Finnish
+	kr38: 'Kirkkoraamattu 1933/1938 (KR38)',
+	kr92: 'Raamattu 1992 (KR92)',
+
+	// Greek
+	tgv: 'Η Αγία Γραφή (TGV)',
+
+	// Hindi
+	hhbd: 'पवित्र बाइबिल (HHBD)',
+
+	// Hungarian
+	uf: 'Magyar újfordítású Biblia (UF)',
+
+	// Malagasy
+	mg1865: 'Malagasy 1865 (MG1865)',
+
+	// Persian
+	pcb: 'کتاب مقدس، ترجمۀ معاصر (PCB)',
+
+	// Russian
+	rst: 'Синодальный перевод (RST)',
+
+	// Slovak
+	kat: 'Katolícky preklad (KAT)',
+
+	// Southern Sotho
+	sso89: 'BIBELE (SSO89)',
+
+	// Urdu
+	urd: 'کِتابِ مُقادّس (URD)',
+
+	// Xhosa
+	xho96: 'IBHAYIBHILE (XHO96)',
+
+	// Zulu
+	zul59: 'Zulu 1959 (ZUL59)',
+}
